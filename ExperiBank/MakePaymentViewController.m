e4cac0295d32d2208f1fa7bc832510e6e0619ac0
aady6497 <adityakiran1982@gmail.com>
2015-09-08 14:10:23 +0530

    Added the touch issue in the repo

--- ExperiBank/MakePaymentViewController.m
+++ ExperiBank/MakePaymentViewController.m
@@ -126,14 +126,14 @@
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-/*
+
 	for(UITouch *touch in touches) {
         [phoneTextField resignFirstResponder];
         [nameTextField resignFirstResponder];
         [amountSlider resignFirstResponder];
         [countryTextField resignFirstResponder];
 	}
-*/
+
 
 }
 
